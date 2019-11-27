7a7c3e6d9c09c82664ccceb74a47b0101ffec9c5
Pedro Entringer <pedro@iconexlog.com.br>
2019-11-27 08:02:17 -0300

    Viewer Page

--- src/pages/Viewer/index.js
+++ src/pages/Viewer/index.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {StatusBar} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import {
   Container,
@@ -11,12 +12,39 @@ import {
   Avatar,
   Details,
   Title,
-  UserName,
+  Text,
   Section,
+  SectionComment,
+  Tags,
+  Tag,
+  Buttons,
+  Button,
+  ButtonText,
+  Bars,
+  Bar,
+  Comment,
+  InputArea,
+  Input,
+  ButtonComment,
+  ButtonCommentText,
 } from './styles';
 
 const Viewer = ({navigation}) => {
   const navigationPost = navigation.getParam('post');
+  navigationPost.likes = 10;
+  navigationPost.dislikes = 5;
+  navigationPost.comments = 10;
+
+  const percent = {
+    likes:
+      (navigationPost.likes /
+        (navigationPost.likes + navigationPost.dislikes)) *
+      100,
+    dislikes:
+      (navigationPost.dislikes /
+        (navigationPost.likes + navigationPost.dislikes)) *
+      100,
+  };
   return (
     <>
       <StatusBar
@@ -35,16 +63,135 @@ const Viewer = ({navigation}) => {
                 </AvatarContent>
                 <Details>
                   <Title>{navigationPost.title}</Title>
-                  <UserName>{navigationPost.user.name}</UserName>
+                  <Text>{navigationPost.user.name}</Text>
                 </Details>
+                <Section>
+                  <FontAwesome5
+                    name={'comments'}
+                    size={20}
+                    color={'#C8C8C8'}
+                    solid
+                  />
+                  <ButtonText style={{color: '#C8C8C8'}}>
+                    {navigationPost.comments}
+                  </ButtonText>
+                </Section>
               </User>
               <Section style={{marginTop: 10}}>
+                <Buttons>
+                  <Button>
+                    <FontAwesome5
+                      name={'thumbs-up'}
+                      size={20}
+                      color={'#24D540'}
+                      solid
+                    />
+                    <ButtonText style={{color: '#24D540'}}>
+                      {navigationPost.likes}
+                    </ButtonText>
+                  </Button>
+                  <Button>
+                    <FontAwesome5
+                      name={'thumbs-down'}
+                      size={20}
+                      color={'#D52424'}
+                      solid
+                    />
+                    <ButtonText style={{color: '#D52424'}}>
+                      {navigationPost.dislikes}
+                    </ButtonText>
+                  </Button>
+                </Buttons>
+
+                <Bars>
+                  <Bar
+                    style={{
+                      width: `${percent.likes}%`,
+                      borderTopRightRadius: 0,
+                      borderBottomRightRadius: 0,
+                    }}
+                  />
+                  <Bar
+                    style={{
+                      width: `${percent.dislikes}%`,
+                      backgroundColor: '#D52424',
+                      borderTopLeftRadius: 0,
+                      borderBottomLeftRadius: 0,
+                    }}
+                  />
+                </Bars>
+              </Section>
+              <Section>
                 <Title>Descrição</Title>
-                <UserName>{navigationPost.description}</UserName>
+                <Text>{navigationPost.description}</Text>
               </Section>
               <Section>
                 <Title>Tags</Title>
-                <UserName>Aqui entram as tags</UserName>
+                <Tags>
+                  <Tag>Node.js</Tag>
+                  <Tag>React Native</Tag>
+                  <Tag>React.js</Tag>
+                  <Tag>Adonis.js</Tag>
+                  <Tag>Express</Tag>
+                  <Tag>Socket.io</Tag>
+                </Tags>
+              </Section>
+              <Section>
+                <Title>Comentários</Title>
+                <SectionComment>
+                  <User>
+                    <AvatarContent>
+                      <Avatar />
+                    </AvatarContent>
+                    <Details>
+                      <Title>{navigationPost.title}</Title>
+                      <Text>{navigationPost.user.name}</Text>
+                    </Details>
+                  </User>
+                  <Comment>
+                    Que video sensacional, me ajudou bastante no estágio.
+                  </Comment>
+                  <Buttons
+                    style={{justifyContent: 'flex-start', marginTop: 10}}>
+                    <Button>
+                      <FontAwesome5
+                        name={'thumbs-up'}
+                        size={20}
+                        color={'#24D540'}
+                        solid
+                      />
+                      <ButtonText style={{color: '#24D540'}}>
+                        {navigationPost.likes}
+                      </ButtonText>
+                    </Button>
+                    <Button>
+                      <FontAwesome5
+                        name={'thumbs-down'}
+                        size={20}
+                        color={'#D52424'}
+                        solid
+                      />
+                      <ButtonText style={{color: '#D52424'}}>
+                        {navigationPost.dislikes}
+                      </ButtonText>
+                    </Button>
+                  </Buttons>
+                </SectionComment>
+              </Section>
+              <Section>
+                <Title>Inserir um Comentário</Title>
+                <InputArea>
+                  <Input
+                    placeholder="Digite seu comentário aqui"
+                    placeholderTextColor="#777777"
+                    multiline={true}
+                    numberOfLines={4}
+                    returnKeyType="send"
+                  />
+                </InputArea>
+                <ButtonComment>
+                  <ButtonCommentText>Adicionar Comentário</ButtonCommentText>
+                </ButtonComment>
               </Section>
             </Main>
           </>
--- src/pages/Viewer/styles.js
+++ src/pages/Viewer/styles.js
@@ -46,11 +46,99 @@ export const Title = styled.Text`
   font-weight: bold;
 `;
 
-export const UserName = styled.Text`
+export const Text = styled.Text`
   color: #777777;
   font-size: 14px;
 `;
 
 export const Section = styled.View`
   padding: 10px 0px;
+`;
+
+export const Tags = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 5px;
+`;
+
+export const Tag = styled.Text`
+  padding: 8px 10px;
+  border-radius: 6px;
+  background-color: #ededed;
+  margin: 5px;
+`;
+
+export const Buttons = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  flex: 1;
+`;
+
+export const Button = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.8,
+})`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 8px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 12px;
+  font-weight: bold;
+  margin-left: 8px;
+`;
+
+export const Bars = styled.View`
+  flex-direction: row;
+  margin-top: 10px;
+`;
+
+export const Bar = styled.View`
+  height: 5px;
+  border-radius: 5px;
+  background-color: #24d540;
+`;
+
+export const Comment = styled.Text`
+  color: #777777;
+  font-size: 14px;
+  margin-top: 10px;
+  text-align: justify;
+`;
+
+export const SectionComment = styled.View`
+  padding: 10px;
+  background-color: #f7f7f7;
+  border-radius: 8px;
+  margin: 5px 0px 5px 0px;
+`;
+
+export const InputArea = styled.KeyboardAvoidingView`
+  align-self: stretch;
+  border-radius: 10px;
+  background-color: #fafafa;
+  border: 1px solid #cdcdcd;
+  margin-top: 10px;
+`;
+
+export const Input = styled.TextInput`
+  color: #777777;
+`;
+
+export const ButtonComment = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.8,
+})`
+  justify-content: center;
+  align-items: center;
+  background-color: #1480b9;
+  border-radius: 6px;
+  padding: 15px 0px;
+  margin-top: 10px;
+`;
+
+export const ButtonCommentText = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
 `;
